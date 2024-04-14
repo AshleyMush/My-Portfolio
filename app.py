ef5e3c60b77377e79671bc263a99ff3eb0788b00
Ashley <tanaka.mush@gmail.com>
2024-04-14 22:52:02 +0100

    Add download link to download button, add templates, create download route and template routes in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, send_from_directory
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 from flask_ckeditor import CKEditor
@@ -29,14 +29,31 @@ def home():
 def project1():
     return render_template('project-1.html')
 
-##
+
 @app.route('/project-2', methods = ['GET', 'POST'])
 def project2():
     return render_template('project-2.html')
 
 @app.route('/project-3', methods = ['GET', 'POST'])
 def project3():
-    return render_template('project-2.html')
+    return render_template('project-3.html')
+
+@app.route('/project-4', methods = ['GET', 'POST'])
+def project4():
+    return render_template('project-4.html')
+
+@app.route('/project-5', methods = ['GET', 'POST'])
+def project5():
+    return render_template('project-5.html')
+
+
+@app.route('/project-6', methods = ['GET', 'POST'])
+def project6():
+    return render_template('project-6.html')
+
+@app.route('/download', methods=['GET','POST'])
+def download():
+    return send_from_directory('static', path="files/cheat_sheet.pf")
 
 
 
